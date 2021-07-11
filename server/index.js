926f6c31acec19ec4af64ff260d2047f30e32a8f
MatildaMared <matildamared@live.se>
2021-07-11 22:09:42 +0200

    initialize project

--- server/index.js
+++ server/index.js
@@ -7,8 +7,16 @@ const app = express();
 
 app.use(cors());
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
   res.json({ "message": "Hello Tildolin!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
