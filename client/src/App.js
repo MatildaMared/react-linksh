81540bba088148ed001f3529403ea4246c1613bf
MatildaMared <matildamared@live.se>
2021-07-11 22:06:00 +0200

    initialize project

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 import "./App.scss";
 
-function App() {
+const App = () => {
+
   const [data, setData] = useState({});
 
   const getData = async () => {
-    const response = await fetch("http://localhost:3001/api");
+    const response = await fetch("/api");
     const data = await response.json();
     setData(data);
-  };
-  
+    console.log(response);
+  }
+
   useEffect(() => {
     getData();
-  });
+  }, []);
 
 	return (
 		<div className="App">
-      <h1>{data.message}</h1>
+      {data.message}
 		</div>
 	);
 }
